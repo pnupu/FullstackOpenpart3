fc822a7d2ecb137635590f597b08522059c2db5e
ilkka lappeteläinen <ilkka.lappetelainen@gmail.com>
2020-07-08 15:06:27 +0300

    commit

---
+++ build/precache-manifest.aa7dafe67404e5155fa1bce593dd0c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7ef45cd56c04b90eb58ae19822697d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aaf0934a3d81c600c46",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "b83d5d237c05fa753d51",
+    "url": "/static/js/2.4d51bd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d51bd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaf0934a3d81c600c46",
+    "url": "/static/js/main.3a34a5d4.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 
 app.use(express.json())
